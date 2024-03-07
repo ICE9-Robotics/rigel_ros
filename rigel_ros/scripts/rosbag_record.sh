c0d6bb66ed742b5ee15a6eecd6960cf1c4e00e47
Anthony Jiang <anthony.zyjiang@outlook.com>
2024-03-07 11:11:47 +0000

    tweaked launch/param files and rosbag record script

--- rigel_ros/scripts/rosbag_record.sh
+++ rigel_ros/scripts/rosbag_record.sh
@@ -1,16 +1,17 @@
 
 rosbag record /camera/color/camera_info \
-              /camera/color/image_raw \
+              /camera/color/image_raw/compressed \
               /camera/depth/camera_info \
               /camera/depth/color/points \
               /camera/depth/image_rect_raw \
               /camera/extrinsics/depth_to_color \
               /c16/lslidar_points \
               /vlp/velodyne_points \
+              /vlp/scan \
+              /msimu/imu/data \
               /msimu/nav/filtered_imu/data \
               /msimu/nav/heading \
               /msimu/nav/odom \
-              /Odometry \
               /odometry/wheel \
               /scout_status \
               /cmd_vel \
